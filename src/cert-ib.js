0e88a2be8c0629fb40c92a02706a7ee3ed434673
kotross <kotross@users.noreply.github.com>
2024-08-09 18:20:41 +0300

    Update cert-ib.js

--- src/cert-ib.js
+++ src/cert-ib.js
@@ -1,1 +1,51 @@
-js
+rootProject.name = 'Mediolanum'
+
+111 kitkat 222
+
+google
+
+yandex
+
+/*
+redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
+
+passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
+
+aws_access_key_id|aws_secret_access_key
+
+public String secretKey;
+public MyThread(String secretKey) {
+    this.secretKey = secretKey;
+*/
+
+www.sberbank.ru
+sberbank.ru
+test.test.sberbank.ru
+
+217.116.48.210
+52.136.254.247
+
+sber
+sberbank
+сбер
+сбербанк
+
+fewfewfewfewfwefew:fewfewfewfewfffewfew
+
+postgres
+mysql
+redis
+amqp
+mongodb
+elasticsearch
+postgresql
+
+
+axa life insurance
+
+axa app
+axa assistance
+axa health insurance
+axa life insurance
+axa login
+axa online
