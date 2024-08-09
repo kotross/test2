4e883075bb978da1a6d93e846a8a0e99cd706716
kotross <kotross@users.noreply.github.com>
2024-08-09 18:20:26 +0300

    Update cert-gib.php

--- src/cert-gib.php
+++ src/cert-gib.php
@@ -1,2 +1,52 @@
 <?php
-1
+rootProject.name = 'Mediolanum'
+
+111 kitkat 222
+
+google
+
+yandex
+
+/*
+redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
+
+passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
+
+aws_access_key_id|aws_secret_access_key
+
+public String secretKey;
+public MyThread(String secretKey) {
+    this.secretKey = secretKey;
+*/
+
+www.sberbank.ru
+sberbank.ru
+test.test.sberbank.ru
+
+217.116.48.210
+52.136.254.247
+
+sber
+sberbank
+сбер
+сбербанк
+
+fewfewfewfewfwefew:fewfewfewfewfffewfew
+
+postgres
+mysql
+redis
+amqp
+mongodb
+elasticsearch
+postgresql
+
+
+axa life insurance
+
+axa app
+axa assistance
+axa health insurance
+axa life insurance
+axa login
+axa online
